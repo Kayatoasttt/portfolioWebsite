267db18a09b7029eeefa40eadb4a67afd36a575c
Kayatoasttt <67277513+Kayatoasttt@users.noreply.github.com>
2024-02-28 15:09:34 +0800

    navigation

--- js/script.js
+++ js/script.js
@@ -27,19 +27,6 @@ window.addEventListener('scroll', function() {
     }
 });
 
-// // sticky navbar
-// window.addEventListener('scroll', function() {
-//     let navbar = document.getElementsByClassName("navbar")[0];
-//     let sticky = navbar.offsetTop;
-
-//     if (window.scrollY >= sticky) {
-//         navbar.classList.add("sticky")
-//       } else {
-//         navbar.classList.remove("sticky");
-//       }
-
-// })
-
 // Navbar
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
@@ -49,50 +36,24 @@ btn.onclick = function () {
     sidebar.classList.toggle('active');
 };
 
-let mainNavLinks = document.querySelectorAll("nav ul li a");
-let mainSection = document.querySelectorAll("main-content");
-
-let lastId;
-let cur=[];
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll("nav ul li a");
 
 window.addEventListener("scroll", event => {
-    let fromTop = window.scrollY;
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
 
-    mainNavLinks.forEach(link => {
-        let section = document.querySelector(link.hash);
-
-        if(section.offsetTop + section.offsetHeight > fromTop){
-            link.classList.add("current");
-        }else{
-            link.classList.remove("current");
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove("current");
+                document.querySelector("nav ul li a[href*=" + id + "]").classList.add("current");
+            })
         }
     })
 })
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Get all the links inside the sidebar
-//     const sidebarLinks = document.querySelectorAll(".sidebar ul li a");
-
-//     // Add click event listeners to each link
-//     sidebarLinks.forEach(function(link) {
-//         link.addEventListener("click", function(event) {
-//             // Prevent the default behavior of the link
-//             event.preventDefault();
-
-//             // Get the target section ID from the link's href attribute
-//             const targetSectionId = link.getAttribute("href").substring(1);
-
-//             // Scroll to the target section smoothly
-//             document.getElementById(targetSectionId).scrollIntoView({
-//                 behavior: "smooth"
-//             });
-
-//             // Optionally, close the sidebar
-//             document.querySelector(".sidebar").classList.remove("active");
-//         });
-//     });
-// });
 
 
 
