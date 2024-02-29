2454430641c22aeb4f675a10f6aa1a728b7d26a5
Kayatoasttt <67277513+Kayatoasttt@users.noreply.github.com>
2024-02-29 12:38:08 +0800

    experience section layout

--- js/script.js
+++ js/script.js
@@ -55,5 +55,86 @@ window.addEventListener("scroll", event => {
     })
 })
 
+// var swiperNodes = "";
+// var pagination = '<div class=swiper-pagination></div>';
+// var next_prev_buttons = '<div class="swiper-button-prev"></div><div class="swiper-button-next"></div>'; 
+// // var scrollbar = '<div class="swiper-scrollbar"></div>';
+// var swiperNodes = swiperNodes.concat(pagination, next_prev_buttons);
+// /* loop throw all swipers on the page */
+// $('.swiper-container').each(function( index ) {
+//   $( this ).append(swiperNodes);
+// });
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    // slidesPerGroup: 3,
+    loop: true,
+    lazy: true,
+    centeredSlides: true,
+    fadeEffect: { crossFade: true },
+    grabCursor : true,
+    // loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    autoplay: {
+              delay: 2500,
+            },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// var mySwiper = new Swiper ('.swiper-container', {
+//     // Optional parameters
+//     spaceBetween: 30,
+//     freeMode: false,
+//     loop: true,
+//     centeredSlides: false,
+//     // Enable lazy loading
+//     lazy: true,
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true
+//     },
+//     keyboard: {
+//       enabled: true,
+//     },
+//     breakpoints: {
+//       0: { /* when window >=0px - webflow mobile landscape/portriat */
+//         slidesPerView: 1,
+//         spaceBetween: 20,
+//       },
+//       767: { /* when window >= 767px - webflow tablet */
+//         slidesPerView: 2,
+//         spaceBetween: 30,
+//       },
+//       988: { /* when window >= 988px - webflow desktop */
+//         slidesPerView: 3,
+//         spaceBetween: 40,
+//       }
+//     },
+//     /* uncomment if you want autoplay slider
+//     autoplay: {
+//       delay: 3000,
+//     },
+//     */
+//     /* uncomment if you want scrollbar
+//      scrollbar: {
+//         el: '.swiper-scrollbar',
+//         hide: true,
+//       },
+//     */
+//   })
+
 
 
